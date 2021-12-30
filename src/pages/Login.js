3e66973fab4ac3a8844e7bef0db1e3f90039842b
Edmond Chen <chen.edmond13@gmail.com>
2021-12-30 15:31:49 -0800

    added login/register page

---
+++ src/pages/Login.js
@@ -0,0 +1,84 @@
+import Nav from "../components/Nav"
+import styled from "styled-components"
+import Footer from "../components/Footer"
+
+const Container = styled.div`
+height: 100vh;
+`
+
+const FormContainer = styled.div`
+display: flex;
+justify-content: center;
+background-color: #264653;
+height: 70%;
+`
+
+const FormTitle = styled.h1``
+
+const Form = styled.form`
+border: 1px solid black;
+width: 100%;
+text-align: center;
+margin: 4em;
+background-color: #feefdd;
+height: 35vh;
+
+@media (min-width: 768px) {
+    width: 40%;
+ }
+
+ @media (min-width: 1200px) {
+    width: 20%;
+ }
+
+`
+
+const FormInput = styled.input`
+height: 25px;
+`
+
+const FormLabel = styled.label`
+`
+
+const TopForm = styled.div`
+margin-top: 1em;
+margin-bottom: 1em;
+`
+
+const AddUser = styled.button`
+border: none;
+padding: 0.5em 2em 0.5em 2em;
+color: white;
+background-color: #ff4000;
+font-weight: 600;
+font-size: 15px;
+font-family: 'PT Sans', sans-serif;
+margin-bottom: 1em;
+`
+
+const Login = () => {
+    return (
+        <Container>
+            <Nav />
+            <FormContainer>
+                <Form>
+                    <FormTitle>Login</FormTitle>
+                    <TopForm>
+                        <FormLabel>Email</FormLabel>
+                        <br></br>
+                        <FormInput type="text"></FormInput>
+                    </TopForm>
+                    <TopForm>
+                        <FormLabel>Password</FormLabel>
+                        <br></br>
+                        <FormInput type="text"></FormInput>
+                    </TopForm>
+                    <AddUser>Login</AddUser>
+                </Form>
+            </FormContainer>
+            <Footer></Footer>
+        </Container>
+    )
+}
+
+export default Login
