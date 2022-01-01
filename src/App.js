9ef4d796eca86e0c92728a60bd6df769e4642c8a
Edmond Chen <chen.edmond13@gmail.com>
2021-12-31 17:52:35 -0800

    routed home page to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import UserDashboard from "./pages/UserDashboard";
+import AddVehicle from "./pages/AddVehicle";
+import VehicleStatus from "./pages/VehicleStatus";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Login />
+    <div>
+      <UserDashboard />
+    </div>
   )
 }
 
