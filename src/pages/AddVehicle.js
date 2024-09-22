ed03f4bc6164b05165e0a0840775f690f9acfa3c
echen124 <chen.edmond13@gmail.com>
2024-09-22 12:54:57 -0700

    changed logic on how vehicles should be added

--- src/pages/AddVehicle.js
+++ src/pages/AddVehicle.js
@@ -17,18 +17,20 @@ const AddVehicle = () => {
     const [date, setDate] = useState("");
     const [color, setColor] = useState("");
     const [vehicleType, setVehicleType] = useState("")
+    const [insuranceProvider, setInsuranceProvider] = useState("")
     const [primaryInfo, setPrimaryInfo] = useState({});
     const [make, setMake] = useState("");
     const [model, setModel] = useState("");
     const [modelYear, setModelYear] = useState("");
+    const [mileage, setMileage] = useState(0);
     const [allVehicles, setAllVehicles] = useState([])
     const ObjectID = require("bson-objectid");
 
-    useEffect(() => {
-        getAllVehicles(sessionStorage.getItem('userKey')).then(d =>
-            //console.log(d.data.data))
-            setAllVehicles(d.data.data.vehicleInfo))
-    }, [])
+    // useEffect(() => {
+    //     getAllVehicles(sessionStorage.getItem('userKey')).then(d =>
+    //         //console.log(d.data.data))
+    //         setAllVehicles(d.data.data.vehicleInfo))
+    // }, [])
 
     const handleVin = (e) => {
         //console.log(e.target.value)
@@ -47,6 +49,14 @@ const AddVehicle = () => {
         setColor(e.target.value);
     }
 
+    const handleMileage = (e) => {
+        setMileage(e.target.value)
+    }
+
+    const handleInsuranceProvider = (e) => {
+        setInsuranceProvider(e.target.value)
+    }
+
     const clearFields = () => {
         setVin("")
         setPlateNumber("")
@@ -56,71 +66,112 @@ const AddVehicle = () => {
     const enterVehicleDetail = (e) => {
         e.preventDefault();
 
-        getAllVehicles()
-            .then(vinArr => {
-                let contains = false;
+        // getAllVehicles()
+        //     .then(vinArr => {
+        //         let contains = false;
 
-                for (let i = 0; i < vinArr.length; i++) {
-                    if (vinArr[i].vin === vin) {
-                        alert("This VIN has already been entered!")
-                        contains = true;
-                        break;
-                    }
-                }
+        //         for (let i = 0; i < vinArr.length; i++) {
+        //             if (vinArr[i].vin === vin) {
+        //                 alert("This VIN has already been entered!")
+        //                 contains = true;
+        //                 break;
+        //             }
+        //         }
 
-                if (!contains) {
+        //         if (!contains) {
 
-                    const newVehicle = {
-                        vin: vin,
-                        // plateNumber: plateNumber,
-                        // insuranceExpiryDate: date,
-                        color: color,
-                        // mileageInformation: [
-                        //     {
-                        //         lastUpdated: moment().format('L'),
-                        //         lastMileage: 0,
-                        //         oilChangeStartInterval: false
-                        //     }
-                        // ],
-                        // warrantyInfo: []
-                    }
+        //             const newVehicle = {
+        //                 vin: vin,
+        //                 // plateNumber: plateNumber,
+        //                 // insuranceExpiryDate: date,
+        //                 color: color,
+        //                 // mileageInformation: [
+        //                 //     {
+        //                 //         lastUpdated: moment().format('L'),
+        //                 //         lastMileage: 0,
+        //                 //         oilChangeStartInterval: false
+        //                 //     }
+        //                 // ],
+        //                 // warrantyInfo: []
+        //             }
 
-                    decodeVIN(vin)
-                        .then(detail => {
-                            setPrimaryInfo(newVehicle)
-                            setVehicleType(detail.Results[21].Value);
-                            setMake(detail.Results[6].Value);
-                            setModel(detail.Results[8].Value);
-                            setModelYear(detail.Results[9].Value);
-                        });
-                }
+        //             decodeVIN(vin)
+        //                 .then(detail => {
+        //                     console.log(detail)
+        //                     setPrimaryInfo(newVehicle)
+        //                     setVehicleType(detail.Results[21].Value);
+        //                     setMake(detail.Results[6].Value);
+        //                     setModel(detail.Results[8].Value);
+        //                     setModelYear(detail.Results[9].Value);
+        //                 });
+        //         }
 
-                const newVehicle = {
-                    vin: vin,
-                    // plateNumber: plateNumber,
-                    // insuranceExpiryDate: date,
-                    color: color,
-                    // mileageInformation: [
-                    //     {
-                    //         lastUpdated: moment().format('L'),
-                    //         lastMileage: 0,
-                    //         oilChangeStartInterval: false
-                    //     }
-                    // ],
-                    // warrantyInfo: []
-                }
+        //         const newVehicle = {
+        //             vin: vin,
+        //             // plateNumber: plateNumber,
+        //             // insuranceExpiryDate: date,
+        //             color: color,
+        //             // mileageInformation: [
+        //             //     {
+        //             //         lastUpdated: moment().format('L'),
+        //             //         lastMileage: 0,
+        //             //         oilChangeStartInterval: false
+        //             //     }
+        //             // ],
+        //             // warrantyInfo: []
+        //         }
 
-                decodeVIN(vin)
-                    .then(detail => {
-                        console.log(detail)
-                        setPrimaryInfo(newVehicle)
-                        setVehicleType(detail.Results[14].Value);
-                        setMake(detail.Results[7].Value);
-                        setModel(detail.Results[9].Value);
-                        setModelYear(detail.Results[10].Value);
-                    });
+        //         decodeVIN(vin)
+        //             .then(detail => {
+        //                 console.log(detail)
+        //                 setPrimaryInfo(newVehicle)
+        //                 setVehicleType(detail.Results[14].Value);
+        //                 setMake(detail.Results[7].Value);
+        //                 setModel(detail.Results[9].Value);
+        //                 setModelYear(detail.Results[10].Value);
+        //             });
 
-            })
+        //     })
+
+        decodeVIN(vin).then(detail => {
+            console.log(detail.Results)
+            const filteredDetails = detail.Results.filter(item =>
+                item.Variable === "Make" ||
+                item.Variable === "Model" ||
+                item.Variable === "Vehicle Type" ||
+                item.Variable === "Model Year"
+            );
+
+            // If you want to create an object or log the filtered results
+            const result = {};
+            filteredDetails.forEach(item => {
+                result[item.Variable] = item.Value;
+            });
+
+            console.log(result); // This will log the filtered object
+
+            const newVehicle = {
+                            vin: vin,
+                            plateNumber: plateNumber,
+                            insuranceExpiryDate: date,
+                            color: color,
+                            insuranceProvider: insuranceProvider
+                            // mileageInformation: [
+                            //     {
+                            //         lastUpdated: moment().format('L'),
+                            //         lastMileage: 0,
+                            //         oilChangeStartInterval: false
+                            //     }
+                            // ],
+                            // warrantyInfo: []
+                        }
+
+            setMake(result["Make"] || ""); // Default to empty string if undefined
+            setModel(result["Model"] || "");
+            setVehicleType(result["Vehicle Type"] || "");
+            setModelYear(result["Model Year"] || "");
+            setPrimaryInfo(newVehicle);
+        });
 
         clearFields();
 
@@ -130,7 +181,7 @@ const AddVehicle = () => {
     return (
         <Container>
             <Nav text="User Dashboard" />
-            <Form enterVehicleDetail={enterVehicleDetail} handleVin={handleVin} vin={vin} handlePlateNum={handlePlateNum} plateNumber={plateNumber} handleColor={handleColor} color={color} handleInsuranceDate={handleInsuranceDate} date={date} vehicleType={vehicleType} primaryInfo={primaryInfo} make={make} model={model} modelYear={modelYear} allVehicles={allVehicles} />
+            <Form enterVehicleDetail={enterVehicleDetail} handleInsuranceProvider = {handleInsuranceProvider} insuranceProvider = {insuranceProvider} handleVin={handleVin} vin={vin} handlePlateNum={handlePlateNum} plateNumber={plateNumber} handleColor={handleColor} handleMileage = {handleMileage} mileage = {mileage} color={color} handleInsuranceDate={handleInsuranceDate} date={date} vehicleType={vehicleType} primaryInfo={primaryInfo} make={make} model={model} modelYear={modelYear} allVehicles={allVehicles} />
             <Footer />
         </Container>
     )
